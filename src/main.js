78f83d3e703f9e1894b95157da343904aaa27e1b
spaceTony9 <bilokon.fw7@gmail.com>
2024-03-04 18:39:15 +0100

    upd

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import './js/sideBar.js'; // Reuta
 // import './js/support/support.js'; //Dobrota
 
 // Reuta
-// import './js/APIservice.js';
-// import './js/book-categories';
-// import './js/book-categories-rendering';
+import './js/APIservice.js';
+import './js/book-categories';
+import './js/book-categories-rendering';
 // import './js/notificationsNotiflix.js';
-// import './js/bestsellers.js';
+import './js/bestsellers.js';
 import './js/shoplist'
-import './js/render-books-section.js';
+// import './js/render-books-section.js';
 import './js/pop-up.js'
 import './js/firebase.js'
 import { CONSTANTS } from './js/constants.js';
