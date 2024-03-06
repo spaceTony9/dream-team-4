50c923cbe36c1f9831637567d3fdaec95c9fa806
Panther1986 <polzovatel77@gmail.com>
2024-03-06 12:07:28 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import './js/support/support.js'; //Dobrota
 // import './js/book-categories-rendering';
 // import './js/notificationsNotiflix.js';
 
-// import './js/bestsellers.js';
+import './js/bestsellers.js';
 import './js/shoplist';
 import './js/render-books-section.js';
 import './js/pop-up.js';
