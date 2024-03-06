f42f3f764de1af37796352a3482cc720c1589cb0
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2024-03-06 20:46:25 +0100

    dark-thema-shoplist

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import './js/support/support.js'; //Dobrota
 // import './js/book-categories-rendering';
 // import './js/notificationsNotiflix.js';
 
-
 // import './js/bestsellers.js';
 
 import './js/render-books-section.js';
