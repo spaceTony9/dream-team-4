857480167976f3de39917b6136e8ee7adc0b884e
spaceTony9 <bilokon.fw7@gmail.com>
2024-03-07 16:27:02 +0100

    updated sign up

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,43 +1,45 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyA3hADlXiNs1sXdUxR8pbsxRLOErTEmqfU',
-  authDomain: 'authorisation-3967f.firebaseapp.com',
-  projectId: 'authorisation-3967f',
-  storageBucket: 'authorisation-3967f.appspot.com',
-  messagingSenderId: '926560198246',
-  appId: '1:926560198246:web:e18f20ad2d2adfa37e97a7',
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyA3hADlXiNs1sXdUxR8pbsxRLOErTEmqfU',
+//   authDomain: 'authorisation-3967f.firebaseapp.com',
+//   projectId: 'authorisation-3967f',
+//   storageBucket: 'authorisation-3967f.appspot.com',
+//   messagingSenderId: '926560198246',
+//   appId: '1:926560198246:web:e18f20ad2d2adfa37e97a7',
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
+// const auth = getAuth(app);
 
-const signUpForm = document.querySelector('#signup-form');
 
-signUpForm.addEventListener('submit', signUp);
+// const signUpForm = await document.querySelector('#signup-form');
+// signUpForm.addEventListener('submit', signUp);
 
-function signUp(event) {
-  event.preventDefault();
-  const name = document.getElementById('signup-name').value;
-  const email = document.getElementById('signup-email').value;
-  const password = document.getElementById('signup-password').value;
+// authentification();
 
-  createUserWithEmailAndPassword(auth, email, password,name)
-    .then(userCredential => {
-      // Signed up successfully
-      const user = userCredential.user
-      userCredential.user.displayName = name;
-      console.log('Signed up:', user);
-    })
-    .catch(error => {
-      // Handle sign-up errors
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.error('Sign up error:', errorMessage);
-    });
-}
+// function signUp(event) {
+//   event.preventDefault();
+//   const name = document.getElementById('signup-name').value;
+//   const email = document.getElementById('signup-email').value;
+//   const password = document.getElementById('signup-password').value;
+
+//   createUserWithEmailAndPassword(auth, email, password, name)
+//     .then(userCredential => {
+//       // Signed up successfully
+//       const user = userCredential.user;
+//       userCredential.user.displayName = name;
+//       console.log('Signed up:', user);
+//     })
+//     .catch(error => {
+//       // Handle sign-up errors
+//       const errorCode = error.code;
+//       const errorMessage = error.message;
+//       console.error('Sign up error:', errorMessage);
+//     });
+// }
--- src/js/signin.js
+++ src/js/signin.js
@@ -1,72 +1,184 @@
 const signinBtn = document.querySelector('.header-btn');
-const sectionContainer = document.querySelector('.section-container')
+const sectionContainer = document.querySelector('.section-container');
 
+const signUpFormContainer = document.querySelector('.sighup-container');
+// signinBtn.addEventListener('click', (e)=> {
 
-signinBtn.addEventListener('click', (e)=> {
+//     if (e.target == e.currentTarget) {
 
-    if (e.target == e.currentTarget) {
-       
-        sectionContainer.classList.remove('is-hidden')
-        
-     return `
-    <div class="section-container">
-      <div class="sighup-container">
-        <button type="button" class="close-form-btn">
-          <svg class="close-form-icon" width="16" height="16">
-            <use href="../img/symbol-defs.svg#icon-close-black"></use>
-          </svg>
-        </button>
-        <form id="signup-form" class="signup-form">
-          <div class="signup-name">
-            <input
-              type="text"
-              id="signup-name"
-              placeholder="Name"
-              class="name-input"
-            />
-          </div>
-          <div class="signup-email">
-            <input
-              type="email"
-              id="signup-email"
-              placeholder="Email"
-              class="email-input"
-            />
-            <svg class="email-icon" width="18" height="18">
-              <use href="../img/symbol-defs.svg#icon-mail"></use>
-            </svg>
-          </div>
-          <div class="signup-password">
-            <input
-              type="password"
-              id="signup-password"
-              placeholder="Password"
-              class="password-input"
-            />
-            <svg class="password-icon" width="18" height="18">
-              <use href="../img/symbol-defs.svg#icon-lock"></use>
-            </svg>
-          </div>
-          <button type="submit" class="signup-btn">Sign Up</button>
-        </form>
-        <div class="sighup-links">
-        <button type="button" class="signup-link">Sign up</button>
-        <button type="button" class="sighin-link">Sign in</button>
-        </div>
+//         sectionContainer.classList.remove('is-hidden')
+
+//      return `
+//     <div class="section-container">
+//       <div class="sighup-container">
+//         <button type="button" class="close-form-btn">
+//           <svg class="close-form-icon" width="16" height="16">
+//             <use href="../img/symbol-defs.svg#icon-close-black"></use>
+//           </svg>
+//         </button>
+//         <form id="signup-form" class="signup-form">
+//           <div class="signup-name">
+//             <input
+//               type="text"
+//               id="signup-name"
+//               placeholder="Name"
+//               class="name-input"
+//             />
+//           </div>
+//           <div class="signup-email">
+//             <input
+//               type="email"
+//               id="signup-email"
+//               placeholder="Email"
+//               class="email-input"
+//             />
+//             <svg class="email-icon" width="18" height="18">
+//               <use href="../img/symbol-defs.svg#icon-mail"></use>
+//             </svg>
+//           </div>
+//           <div class="signup-password">
+//             <input
+//               type="password"
+//               id="signup-password"
+//               placeholder="Password"
+//               class="password-input"
+//             />
+//             <svg class="password-icon" width="18" height="18">
+//               <use href="../img/symbol-defs.svg#icon-lock"></use>
+//             </svg>
+//           </div>
+//           <button type="submit" class="signup-btn">Sign Up</button>
+//         </form>
+//         <div class="sighup-links">
+//         <button type="button" class="signup-link">Sign up</button>
+//         <button type="button" class="sighin-link">Sign in</button>
+//         </div>
+//       </div>
+//     </div>
+
+//     `;
+//     }
+
+// })
+
+signinBtn.addEventListener('click', e => {
+  sectionContainer.classList.remove('is-hidden');
+  fillInSingUpForm();
+});
+
+signUpFormContainer.addEventListener('click', e => {
+  const closeSighupBtn = document.querySelector('.close-form-btn');
+  closeSighupBtn.addEventListener('click', () => {
+    sectionContainer.classList.add('is-hidden');
+  });
+  if (e.target.getAttribute('class') === 'sighin-link') {
+    sectionContainer.classList.add('log-in-container');
+    fillInSingInForm();
+  } else if (e.target.getAttribute('class') === 'signup-link') {
+    fillInSingUpForm();
+  }
+});
+
+// signinBtn.addEventListener('click', e => {
+//   sectionContainer.classList.remove('is-hidden');
+//   fillInSingUpForm();
+//   const closeSighupBtn = document.querySelector('.close-form-btn');
+//
+//   const signupLink = document.querySelector('.signup-link');
+//   const signinLink = document.querySelector('.sighin-link');
+//   signinLink.addEventListener('click', fillInSingInForm);
+//   signupLink.addEventListener('click', fillInSingUpForm);
+//   // return `
+//   // <div class="section-container">
+//   // <div class="sighup-container">
+//   //   ${fillInSingUpForm()}
+//   //   </div>
+//   // </div>`;
+// });
+
+function fillInSingUpForm() {
+  signUpFormContainer.innerHTML = `
+  <button type="button" class="close-form-btn ">
+  <span><svg class="close-form-icon " width="16" height="16">
+  <use href="../img/symbol-defs.svg#icon-close-black"></use></svg></span>
+  
+  </button>
+          <form id="signup-form" class="signup-form">
+            <div class="signup-name">
+              <input
+                type="text"
+                id="signup-name"
+                placeholder="Name"
+                class="name-input"
+              />
+            </div>
+            <div class="signup-email">
+              <input
+                type="email"
+                id="signup-email"
+                placeholder="Email"
+                class="email-input"
+              />
+              <svg class="email-icon" width="18" height="18">
+                <use href="../img/symbol-defs.svg#icon-mail"></use>
+              </svg>
+            </div>
+            <div class="signup-password">
+              <input
+                type="password"
+                id="signup-password"
+                placeholder="Password"
+                class="password-input"
+              />
+              <svg class="password-icon" width="18" height="18">
+                <use href="../img/symbol-defs.svg#icon-lock"></use>
+              </svg>
+            </div>
+            <button type="submit" class="signup-btn">Sign Up</button>
+          </form>
+          <div class="sighup-links">
+          <button type="button" class="signup-link">Sign up</button>
+          <button type="button" class="sighin-link">Sign in</button>`;
+}
+function fillInSingInForm() {
+  signUpFormContainer.innerHTML = ` <button type="button" class="close-form-btn">
+      <svg class="close-form-icon" width="16" height="16">
+        <use href="../img/symbol-defs.svg#icon-close-black"></use>
+      </svg>
+    </button>
+    <form id="signup-form" class="signup-form">
       </div>
-    </div>
-    
-    `;
-    }
-    
-})
-const closeSighupBtn = document.querySelector('.close-form-btn')
-closeSighupBtn.addEventListener('click', ()=> {
-    sectionContainer.classList.add('is-hidden')
-})
+      <div class="signup-email">
+        <input
+          type="email"
+          id="signup-email"
+          placeholder="Email"
+          class="email-input"
+        />
+        <svg class="email-icon" width="18" height="18">
+          <use href="../img/symbol-defs.svg#icon-mail"></use>
+        </svg>
+      </div>
+      <div class="signup-password">
+        <input
+          type="password"
+          id="signup-password"
+          placeholder="Password"
+          class="password-input"
+        />
+        <svg class="password-icon" width="18" height="18">
+          <use href="../img/symbol-defs.svg#icon-lock"></use>
+        </svg>
+      </div>
+      <button type="submit" class="signup-btn">Sign Up</button>
+    </form>
+    <div class="sighup-links">
+    <button type="button" class="signup-link">Sign up</button>
+    <button type="button" class="sighin-link">Sign in</button>`;
+}
 
-function signupTemplate(){
-    return `
+function signupTemplate() {
+  return `
     <div class="sighup-container">
     <button type="button" class="close-form-btn">
       <svg class="close-form-icon" width="16" height="16">
@@ -110,62 +222,48 @@ function signupTemplate(){
     <button type="button" class="signup-link">Sign up</button>
     <button type="button" class="sighin-link">Sign in</button>
     </div>
-  </div>`
+  </div>`;
 }
 
-function signinTemplate(){
-    return `
-   
-  <div class="sighup-container">
-  <button type="button" class="close-form-btn">
-  <svg class="close-form-icon" width="16" height="16">
-    <use href="../img/symbol-defs.svg#icon-close-black"></use>
-  </svg>
-</button>
-    <form id="signup-form" class="signup-form">
-      <div class="signup-email">
-        <input
-          type="email"
-          id="signup-email"
-          placeholder="Email"
-          class="email-input"
-        />
-        <svg class="email-icon" width="18" height="18">
-          <use href="../img/symbol-defs.svg#icon-mail"></use>
-        </svg>
-      </div>
-      <div class="signup-password">
-        <input
-          type="password"
-          id="signup-password"
-          placeholder="Password"
-          class="password-input"
-        />
-        <svg class="password-icon" width="18" height="18">
-          <use href="../img/symbol-defs.svg#icon-lock"></use>
-        </svg>
-      </div>
-      <button type="submit" class="signup-btn">Sign Ip</button>
-    </form>
-    <div class="sighup-links">
-    <button type="button" class="signup-link">Sign up</button>
-    <button type="button" class="sighin-link">Sign in</button>
-    </div>
-  </div>
+// function signinTemplate() {
+//   return `
 
-    `
-};
+//   <div class="sighup-container">
+//   <button type="button" class="close-form-btn">
+//   <svg class="close-form-icon" width="16" height="16">
+//     <use href="../img/symbol-defs.svg#icon-close-black"></use>
+//   </svg>
+// </button>
+//     <form id="signup-form" class="signup-form">
+//       <div class="signup-email">
+//         <input
+//           type="email"
+//           id="signup-email"
+//           placeholder="Email"
+//           class="email-input"
+//         />
+//         <svg class="email-icon" width="18" height="18">
+//           <use href="../img/symbol-defs.svg#icon-mail"></use>
+//         </svg>
+//       </div>
+//       <div class="signup-password">
+//         <input
+//           type="password"
+//           id="signup-password"
+//           placeholder="Password"
+//           class="password-input"
+//         />
+//         <svg class="password-icon" width="18" height="18">
+//           <use href="../img/symbol-defs.svg#icon-lock"></use>
+//         </svg>
+//       </div>
+//       <button type="submit" class="signup-btn">Sign Up</button>
+//     </form>
+//     <div class="sighup-links">
+//     <button type="button" class="signup-link">Sign up</button>
+//     <button type="button" class="sighin-link">Sign in</button>
+//     </div>
+//   </div>
 
-const signupLink = document.querySelector('.sighup-link');
-const signinLink = document.querySelector('.sighin-link');
-
-
-
-
-signinLink.addEventListener('click',  ()=>{
-    
-        sectionContainer.innerHTML = signinTemplate();
-    
-
-} );
-
+//     `;
+// }
